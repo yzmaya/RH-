91b9e9f540c4b4ba8bf3d71c4a8e4a4231cf2970
Néstor Yzmaya <nestor.tarinda@gmail.com>
2018-10-11 17:35:36 -0500

    modificaciones swb

--- js/sesion.js
+++ js/sesion.js
@@ -11,26 +11,7 @@
   };
   firebase.initializeApp(config);
 
-  $(document).ready(function () {
-
-            (function ($) {
-
-                $('#filtrar').keyup(function () {
-
-                    var rex = new RegExp($(this).val(), 'i');
-                  
-                    $('.buscar tr').hide();
-                    $('.buscar tr').filter(function () {
-                        return rex.test($(this).text());
-                          
-                    }).show();
-   
-                });
-
-            }(jQuery));
-             
-
-        });
+ 
 
   var chicas = firebase.database().ref('meta/');
 
